c3cc64549ec2bbe0182d7a1c1fc768abdb686cc7
hwangsebin-2 <sbsbin99@gmail.com>
2022-01-05 00:07:20 +0900

    Lv1_직사각형별찍기 commit

--- src/Lv1_직사각형별찍기.java
+++ src/Lv1_직사각형별찍기.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Lv1_직사각형별찍기 {
 	public static void main(String[] args) {
+		
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
